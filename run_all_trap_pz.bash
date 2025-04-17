b79cf6a772f1aa4a8f7993d0d1f94587986d1fdd
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2025-04-17 19:56:39 +0900

    fix bash

--- run_all_trap_pz.bash
+++ run_all_trap_pz.bash
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-python train_ippo_lstm_trap.py --track --cuda False --seed=1 --wandb-group=no &
-python train_ippo_lstm_trap.py --track --cuda False --seed=1 --wandb-group=cognitive &
-python train_ippo_lstm_trap.py --track --cuda False --seed=1 --wandb-group=affective &
-python train_ippo_lstm_trap.py --track --cuda False --seed=1 --wandb-group=full &
+python train_ippo_lstm_trap.py --track --seed=1 --wandb-group=no &
+python train_ippo_lstm_trap.py --track --seed=1 --wandb-group=cognitive &
+python train_ippo_lstm_trap.py --track --seed=1 --wandb-group=affective &
+python train_ippo_lstm_trap.py --track --seed=1 --wandb-group=full &
 
-python train_ippo_lstm_trap.py --track --cuda False --seed=2 --wandb-group=no &
-python train_ippo_lstm_trap.py --track --cuda False --seed=2 --wandb-group=cognitive &
-python train_ippo_lstm_trap.py --track --cuda False --seed=2 --wandb-group=affective &
-python train_ippo_lstm_trap.py --track --cuda False --seed=2 --wandb-group=full &
+python train_ippo_lstm_trap.py --track --seed=2 --wandb-group=no &
+python train_ippo_lstm_trap.py --track --seed=2 --wandb-group=cognitive &
+python train_ippo_lstm_trap.py --track --seed=2 --wandb-group=affective &
+python train_ippo_lstm_trap.py --track --seed=2 --wandb-group=full &
 
-python train_ippo_lstm_trap.py --track --cuda False --seed=3 --wandb-group=no &
-python train_ippo_lstm_trap.py --track --cuda False --seed=3 --wandb-group=cognitive &
-python train_ippo_lstm_trap.py --track --cuda False --seed=3 --wandb-group=affective &
-python train_ippo_lstm_trap.py --track --cuda False --seed=3 --wandb-group=full &
+python train_ippo_lstm_trap.py --track --seed=3 --wandb-group=no &
+python train_ippo_lstm_trap.py --track --seed=3 --wandb-group=cognitive &
+python train_ippo_lstm_trap.py --track --seed=3 --wandb-group=affective &
+python train_ippo_lstm_trap.py --track --seed=3 --wandb-group=full &
 
-python train_ippo_lstm_trap.py --track --cuda False --seed=4 --wandb-group=no &
-python train_ippo_lstm_trap.py --track --cuda False --seed=4 --wandb-group=cognitive &
-python train_ippo_lstm_trap.py --track --cuda False --seed=4 --wandb-group=affective &
-python train_ippo_lstm_trap.py --track --cuda False --seed=4 --wandb-group=full &
+python train_ippo_lstm_trap.py --track --seed=4 --wandb-group=no &
+python train_ippo_lstm_trap.py --track --seed=4 --wandb-group=cognitive &
+python train_ippo_lstm_trap.py --track --seed=4 --wandb-group=affective &
+python train_ippo_lstm_trap.py --track --seed=4 --wandb-group=full &
 
-python train_ippo_lstm_trap.py --track --cuda False --seed=5 --wandb-group=no &
-python train_ippo_lstm_trap.py --track --cuda False --seed=5 --wandb-group=cognitive &
-python train_ippo_lstm_trap.py --track --cuda False --seed=5 --wandb-group=affective &
-python train_ippo_lstm_trap.py --track --cuda False --seed=5 --wandb-group=full &
+python train_ippo_lstm_trap.py --track --seed=5 --wandb-group=no &
+python train_ippo_lstm_trap.py --track --seed=5 --wandb-group=cognitive &
+python train_ippo_lstm_trap.py --track --seed=5 --wandb-group=affective &
+python train_ippo_lstm_trap.py --track --seed=5 --wandb-group=full &
 
 wait
 
