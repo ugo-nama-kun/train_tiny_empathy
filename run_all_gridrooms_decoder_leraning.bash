b858c6a3f19885394a53137a0b686b847340e0dc
Naoto Yoshida <n-yoshida@isi.imi.i.u-tokyo.ac.jp>
2025-04-24 14:16:39 +0900

    update

--- run_all_gridrooms_decoder_leraning.bash
+++ run_all_gridrooms_decoder_leraning.bash
@@ -1,8 +1,8 @@
 
 for i in `seq 0 19`
 do
-  python train_grid_rooms_decoder_learning.py --cuda --track --decoding-mode="full" --enable_learning &
-  python train_grid_rooms_decoder_learning.py --cuda --track --decoding-mode="affect"  --enable_learning &
+  python train_grid_rooms_decoder_learning.py --track --decoding-mode="full" --enable_learning &
+  python train_grid_rooms_decoder_learning.py --track --decoding-mode="affect"  --enable_learning &
   python train_grid_rooms_decoder_learning.py --track --decoding-mode="full" &
   python train_grid_rooms_decoder_learning.py --track --decoding-mode="affect" &
   wait
